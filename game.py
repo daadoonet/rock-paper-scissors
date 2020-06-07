7cb751f46e8334b738bd3df2930ff29afe992f92
daadoo <daadoo.net@gmail.com>
2020-06-08 01:21:17 +0430

    get better
    add some TODO 
    get some faster
    get more beautiful

--- game.py
+++ game.py
@@ -6,32 +6,41 @@ os.system('cls')
 
 
 # defines the game logic
-def logic(p1, p2):
+def logic(p1, p2, mode):
+    #set outputs
+    if mode == "m":
+        w1 = "player1 wins!!"
+        w2 = "player2 wins!!"
+    elif mode == "s":
+        w1 = "you win :)"
+        w2 = "computer wins :("
+
     # checks if the input is valid or not
-    if not ((p1 == "1" or p1 == "2" or p1 == "3") and (p2 == "1" or p2 == "2" or p2 == "3")):
+    if not ((p1 == "1" or "2" or "3") and (p2 == "1" or "2" or "3")):
         print("something went wrong")
         main()
     else:
+        #TODO make if's shorter
         if p1 == p2:
             print("that's a tie ...")
 
         if p1 == "1":
             if p2 == "2":
-                print("player2 wins!!")
+                print(w2)
             elif p2 == "3":
-                print("player1 wins!!")
+                print(w1)
 
         if p1 == "2":
             if p2 == "1":
-                print("player1 wins!!")
+                print(w1)
             elif p2 == "3":
-                print("player2 wins!!")
+                print(w2)
 
         if p1 == "3":
             if p2 == "1":
-                print("player2 wins!!")
+                print(w2)
             elif p2 == "2":
-                print("player1 wins!!")
+                print(w1)
 
 
 # deletes the previous line
@@ -68,14 +77,13 @@ def main():
     gameMode = input("multiplayer or single player? m/s \n").lower()
 
     if gameMode == "m":
-
         # gets palyers moves
         player1 = input("player 1 choose your move number:  ").lower()
         deleteLine()
         player2 = input("player 2 choose your move number:  ").lower()
         deleteLine()
 
-        logic(player1, player2)
+        logic(player1, player2, "m")
         exit()
 
     elif gameMode == "s":
@@ -85,7 +93,7 @@ def main():
         player2 = str(random.randint(1, 3))
         print(f"Computer move was: {player2}")
 
-        logic(player1, player2)
+        logic(player1, player2, "s")
         exit()
     else:
         os.system('cls')
@@ -94,3 +102,5 @@ def main():
 
 # runs the program
 main()
+
+#TODO add point counter
